e20feace9a7944c37a5834fda2904560071f8a76
marina-markova <mvmarkova1990@abv.bg>
2020-05-29 16:01:48 +0300

    First Upload

---
+++ src/test/java/project/a/ProjectA1ApplicationTests.java
@@ -0,0 +1,16 @@
+package project.a;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectA1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
